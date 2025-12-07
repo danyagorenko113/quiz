c9ef397bc5d8d6605ca2f92211cb819afc64ddf7
danyagorenko113 <dgorenko115@gmail.com>
2025-12-07 05:37:58 +0300

    Create footer.tsx

---
+++ components/footer.tsx
@@ -0,0 +1,9 @@
+export function Footer() {
+  return (
+    <footer className="border-t border-zinc-800 py-6 mt-auto">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <p className="text-center text-gray-500 text-sm font-light">Made with❤️ by danyagorenko - 2025</p>
+      </div>
+    </footer>
+  )
+}
