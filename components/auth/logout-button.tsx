d8ab99e3f7e14c1ec51c37cfb0b39b179e86a21e
danyagorenko113 <dgorenko115@gmail.com>
2025-12-28 20:19:25 +0300

    Create logout-button.tsx

---
+++ components/auth/logout-button.tsx
@@ -0,0 +1,17 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import { LogOut } from "lucide-react"
+
+export default function LogoutButton() {
+  return (
+    <Button
+      onClick={() => (window.location.href = "/api/auth/logout")}
+      variant="outline"
+      className="border-[#fc4839] text-[#fc4839] hover:bg-[#fc4839] hover:text-white"
+    >
+      <LogOut className="mr-2 h-5 w-5" />
+      Log Out
+    </Button>
+  )
+}
