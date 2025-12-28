0ac2b9b9cd770915ab82781533975c4de75385f1
danyagorenko113 <dgorenko115@gmail.com>
2025-12-28 19:47:38 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
-import { getServerSession } from "next-auth"
-import { authOptions } from "@/lib/auth"
 import { LandingPage } from "@/components/landing-page"
 
-export default async function Home() {
-  const session = await getServerSession(authOptions)
-
-  return <LandingPage session={session} />
+export default function Home() {
+  return <LandingPage />
 }
