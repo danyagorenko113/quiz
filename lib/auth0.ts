0691918e8ccecf08bec51324bf9ffc99b4af1b6f
danyagorenko113 <dgorenko115@gmail.com>
2025-12-28 21:29:22 +0300

    Update auth0.ts

--- lib/auth0.ts
+++ lib/auth0.ts
@@ -1,2 +1,2 @@
-// Re-export commonly used Auth0 functions from server module
-export { getSession, getAccessToken, withApiAuthRequired, withPageAuthRequired } from "@auth0/nextjs-auth0/server"
+// Re-export commonly used Auth0 functions
+export { getSession, getAccessToken, withApiAuthRequired, withPageAuthRequired } from "@auth0/nextjs-auth0"
